eda63281b6ec331289cfbe8aa87ff687479920f1
Andrew Naoum <andrew.naoum@harrison.ai>
2022-08-30 00:29:06 +0000

    check env var set

--- scripts/awskube.sh
+++ scripts/awskube.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "${CLUSTER_NAME}" ]; then
+    >&2 echo "Must set environment variable CLUSTER_NAME"
+    exit 1
+fi
+
 echo "Updating kubeconfig for cluster ${CLUSTER_NAME}"
 
 aws eks update-kubeconfig --name "${CLUSTER_NAME}"
